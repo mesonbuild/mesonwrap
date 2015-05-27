f01aea3a929dfeaa5e0a8bec631ddd944ea80741
Jussi Pakkanen <jpakkane@gmail.com>
2015-05-27 18:36:31 +0300

    Fine tuning and add update method.

--- wrapweb/__init__.py
+++ wrapweb/__init__.py
@@ -13,20 +13,20 @@
 # limitations under the License.
 
 from flask import Flask, jsonify, request, Response
+import re
 import wrapdb
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-db = wrapdb.WrapDatabase('/tmp')
 
-from wrapweb.models import *
+db_directory = '/tmp'
+querydb = wrapdb.WrapDatabase(db_directory)
+db_updater = wrapmanager.WrapManager(db_directory)
 
-@app.route("/projects", defaults={"project": None})
 @app.route("/projects/<project>")
-def get_project(project=''):
-    out = {"output": "ok", "projects": []}
-    for prj in db.name_search(project):
-        out["projects"].append(prj.to_dict())
+def get_project(project):
+    out = {"output": "ok", "versions": []}
+    for prj in db.get_versions(project):
+        out["versions"].append(prj.to_dict())
     httpcode = 200
 
     jsonout = jsonify(out)
@@ -34,27 +34,44 @@ def get_project(project=''):
     return jsonout
 
 @app.route("/projects/<project>/get_wrap", methods=['GET'])
-def get_wrap(project):
-    branch=request.args["branch"]
-    revision=int(request.args["revision"])
-    result = db.get_wrap(project, branch, revision)
-    if result is None:
-        out = {"output": "notok", "error": "No such entry"}
-        jsonout = jsonify(out)
-        jsonout.status_code = 500
-        return jsonout
-    else:
-        return Response(result, mimetype="text/plain")
-
 @app.route("/projects/<project>/get_zip", methods=['GET'])
 def get_wrap(project):
     branch=request.args["branch"]
     revision=int(request.args["revision"])
-    result = db.get_zip(project, branch, revision)
+    if request.path.endswith("/get_wrap"):
+        result = db.get_wrap(project, branch, revision)
+        mtype = 'text/plain'
+    else:
+        result = db.get_zip(project, branch, revision)
+        mtype = 'application/zip'
     if result is None:
         out = {"output": "notok", "error": "No such entry"}
         jsonout = jsonify(out)
         jsonout.status_code = 500
         return jsonout
     else:
-        return Response(result, mimetype="application/zip")
+        return Response(result, mimetype=mtype)
+
+# Change to match whatever github expects to get. Also verify password/IP/whatever.
+@app.route('/update/<project>/<branch>')
+def update_project(project, branch):
+    if not re.fullmatch('[a-z0-9._]', project):
+        out = {"output": "notok", "error": "Invalid project name"}
+        jsonout = jsonify(out)
+        jsonout.status_code = 500
+        return jsonout
+    if not re.fullmatch('[a-z0-9._]', branch):
+        out = {"output": "notok", "error": "Invalid branch name"}
+        jsonout = jsonify(out)
+        jsonout.status_code = 500
+        return jsonout
+    # FIXME, should launch in the background instead. This will now block
+    # until branching is finished.
+    try:
+        db_updater.update(project, branch)
+    except Exception:
+        out = {"output": "notok", "error": "Wrap generation failed."}
+        jsonout = jsonify(out)
+        jsonout.status_code = 500
+        return jsonout
+    return jsonify({'output': 'ok'})
