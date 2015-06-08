1e268e6f030a910b8d9969c67639abd45f01e8d0
Jussi Pakkanen <jpakkane@gmail.com>
2015-06-09 00:11:27 +0300

    License clarification.

--- tools/repoinit.py
+++ tools/repoinit.py
@@ -38,6 +38,8 @@ def initialize(reponame):
     ofile = open('readme.txt', 'w')
     ofile.write('''This repository contains a Meson build definition for project %s.
 
+All files in this repository have the same license as the original project.
+
 For more information please see http://mesonbuild.com.
 ''' % reponame)
     ofile.close()
