9c74954231af347e18ed1350f4d62a89efb4e6d5
Aleksey Filippov <alekseyf@google.com>
2018-05-22 15:48:15 +0100

    Refactor github_hook()

--- wrapweb/hook.py
+++ wrapweb/hook.py
@@ -21,59 +21,56 @@ from wrapweb.app import APP
 from wrapweb import db
 
 
-RESTRICTED_PROJECTS = ['mesonbuild/meson', 'mesonbuild/wrapweb', 'mesonbuild/meson-ci']
+RESTRICTED_PROJECTS = [
+    'mesonbuild/meson',
+    'mesonbuild/wrapweb',
+    'mesonbuild/meson-ci',
+]
+
+
+def json_ok():
+    jsonout = flask.jsonify({'output': 'ok'})
+    jsonout.status_code = 200
+    return jsonout
+
+
+def json_error(code, message):
+    jsonout = flask.jsonify({'output': 'notok', 'error': message})
+    jsonout.status_code = code
+    return jsonout
 
 
 @APP.route('/github-hook', methods=['POST'])
 def github_hook():
-    if not flask.request.headers.get('User-Agent').startswith('GitHub-Hookshot/'):
-        jsonout = flask.jsonify({'output': 'notok', 'error': 'Not a GitHub hook'})
-        jsonout.status_code = 401
-        return jsonout
+    headers = flask.request.headers
+    if not headers.get('User-Agent').startswith('GitHub-Hookshot/'):
+        return json_error(401, 'Not a GitHub hook')
     signature = 'sha1=%s' % hmac.new(APP.config['SECRET_KEY'].encode('utf-8'),
                                      flask.request.data, hashlib.sha1).hexdigest()
-    if flask.request.headers.get('X-Hub-Signature') != signature:
-        jsonout = flask.jsonify({'output': 'notok', 'error': 'Not a valid secret key'})
-        jsonout.status_code = 401
-        return jsonout
-    if flask.request.headers.get('X-Github-Event') != 'pull_request':
-        jsonout = flask.jsonify({'output': 'notok', 'error': 'Not a Pull Request hook'})
-        jsonout.status_code = 405
-        return jsonout
+    if headers.get('X-Hub-Signature') != signature:
+        return json_error(401, 'Not a valid secret key')
+    if headers.get('X-Github-Event') != 'pull_request':
+        return json_error(405, 'Not a Pull Request hook')
     d = flask.request.get_json()
     base = d['pull_request']['base']
     if not base['repo']['full_name'].startswith('mesonbuild/'):
-        jsonout = flask.jsonify({'output': 'notok', 'error': 'Not a mesonbuild project'})
-        jsonout.status_code = 406
-        return jsonout
+        return json_error(406, 'Not a mesonbuild project')
     if base['repo']['full_name'] in RESTRICTED_PROJECTS:
-        jsonout = flask.jsonify({'output': 'notok',
-                                 'error': "We don't run hook for restricted project names"})
-        jsonout.status_code = 406
-        return jsonout
+        return json_error(406, "We don't run hook for restricted project names")
     if d['action'] == 'closed' and d['pull_request']['merged']:
         project = base['repo']['name']
         branch = base['ref']
         repo_url = base['repo']['clone_url']
         if branch == 'master':
-            out = {'output': 'notok', 'error': 'No bananas for you'}
-            httpcode = 406
-        else:
-            out = {'output': 'ok'}
-            httpcode = 200
-            db_updater = db.get_update_db()
-            # FIXME, should launch in the background instead. This will now block
-            # until branching is finished.
-            try:
-                db_updater.update_db(project, repo_url, branch)
-            except Exception as e:
-                out = {'output': 'notok', 'error': 'Wrap generation failed. %s' % e}
-                httpcode = 500
+            return json_error(406, 'No bananas for you')
+        db_updater = db.get_update_db()
+        # FIXME, should launch in the background instead. This will now block
+        # until branching is finished.
+        try:
+            db_updater.update_db(project, repo_url, branch)
+            return json_ok()
+        except Exception as e:
+            return json_error(500, 'Wrap generation failed. %s' % e)
     else:
         APP.logger.warning(flask.request.data)
-        out = {'output': 'notok', 'error': 'We got hook which is not merged pull request'}
-        httpcode = 417
-
-    jsonout = flask.jsonify(out)
-    jsonout.status_code = httpcode
-    return jsonout
+        return json_error(417, 'We got hook which is not merged pull request')
