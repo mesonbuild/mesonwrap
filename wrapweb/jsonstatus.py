8a7b3087460e774f354ee1fb791f4c6ebcab9f55
Lisa White <lisq@google.com>
2020-03-15 20:08:38 +0000

    Fix redefined-outer-name in wrapweb.jsonstatus

--- wrapweb/jsonstatus.py
+++ wrapweb/jsonstatus.py
@@ -20,9 +20,9 @@ class WrapWebError(Exception):
         return dict(output='notok', error=self.message)
 
 
-def handle_wrap_web_error(error: WrapWebError):
-    jsonout = flask.jsonify(error.to_dict())
-    jsonout.status_code = error.status_code
+def handle_wrap_web_error(err: WrapWebError, /):
+    jsonout = flask.jsonify(err.to_dict())
+    jsonout.status_code = err.status_code
     return jsonout
 
 
